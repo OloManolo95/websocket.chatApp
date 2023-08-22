427f1cb09cf9cdb567779819f9fffbb60903dc68
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-08-22 14:04:18 +0200

    add addMessage function and autoComplete attribute to chat

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 {
   "use strict";
-
   // HTML references
   const loginForm = document.querySelector('#welcome-form');
   const messagesSection = document.querySelector('#messages-section');
@@ -8,14 +7,11 @@
   const addMessageForm = document.querySelector('#add-messages-form');
   const userNameInput = document.querySelector('#username');
   const messageContentInput = document.querySelector('#message-content');
-
   // Global vars
   let userName = '';
-
   // Functions
   const login = (e) => {
     e.preventDefault();
-
     if (!userNameInput.value) {
       alert('Please enter a username');
     } else {
@@ -25,7 +21,23 @@
     }
   };
 
+  const addMessage = (author, content) => {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+    messagesList.appendChild(message);
+  }
+
   const sendMessage = (e) => {
+    e.preventDefault();
+
     if (!messageContentInput.value) {
       alert('Please enter a message');
     } else {
